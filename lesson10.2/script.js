5da917d554eb7a8c3d4cf8f79effa7d897dc2e5e
Abilkasov Dias <abilkasovd@yandex.ru>
2019-09-13 17:34:21 +0600

    lesson10.2

---
+++ lesson10.2/script.js
@@ -0,0 +1,71 @@
+function DomElement(selector, height, width, bg, fontSize) {
+    this.selector = selector.split('');
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+}
+
+let divElement = new DomElement('.main', 100, 100, 'red', 16);
+let pElement = new DomElement('#main', 220, 220, 'red', 16);
+
+DomElement.prototype.addElement = function() {
+    for(let i = 0; i < this.selector.length; i++) {
+
+        if(this.selector[i] === '.') {
+            let div = document.createElement('div');
+            div.style.height = this.height + 'px';
+            div.style.width = this.width + 'px';
+            div.style.backgroundColor = this.bg;
+            div.style.fontSize = this.fontSize + 'px';
+            div.style.position = 'absolute';
+            div.innerHTML = 'Hi i am <strong>div</strong> tag';
+            div.classList.add(this.selector.join('')); 
+            document.body.append(div);
+        } else if(this.selector[i] === '#') {
+            let p = document.createElement('p');
+            p.style.height = this.height + 'px';
+            p.style.width = this.width + 'px';
+            p.style.backgroundColor = this.bg;
+            p.style.fontSize = this.fontSize + 'px';
+            p.innerHTML = 'Hi i am <strong>p</strong> tag';
+            document.body.append(p);
+        }
+
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    divElement.addElement();
+
+    let element = document.querySelector('div');
+    let left = 0,
+        top = 0;
+
+    document.addEventListener('keydown', function(event) {
+
+        if(event.keyCode === 37) {
+            element.style.left = left + 'px';
+            left += 10;
+        } 
+        if(event.keyCode === 38) {
+            element.style.top = top + 'px';
+            top += 10;
+        } 
+        if(event.keyCode === 39) {
+            element.style.left = left + 'px';
+            left -= 10;
+        } 
+        if(event.keyCode === 40){
+            element.style.top = top + 'px';
+            top -= 10;
+        }
+    
+    });
+});
+
+
+
+
+
+
